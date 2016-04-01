b28b62a4f9a8035c2b7059b722106d5e6f971d47
Parviz Aliyev <byaliyev@outlook.com>
2016-04-01 18:42:10 +0500

    parçalandı

--- index.php
+++ index.php
@@ -1,54 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>JobPortal</title>
-	<!-- Normalise css -->
-	<link rel="stylesheet" href="css/normalise.css">
-	<!-- Bootstrap css -->
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<!-- FornAwesome css -->
-	<link rel="stylesheet" href="css/font-awesome.css">
-	<!-- Style css -->
-	<link rel="stylesheet" href="css/responsive.css">
-	<link rel="stylesheet" href="css/style.css">
-
-	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
-</head>
-<body>
-
-	<!-- Header Start -->
-	<header id="header">
-		<div class="container">
-
-			<!-- Header Reklam -->
-			<div class="headerReklam">
-				<div class="headerReklami">
-					<iframe border="0" frameborder="0" height="120" marginheight="0" marginwidth="0" scrolling="no" src="http://b.turbo.az/r/?p=32" width="255"></iframe>
-				</div>
-				<div class="headerReklami">
-					<iframe border="0" frameborder="0" height="120" marginheight="0" marginwidth="0" scrolling="no" src="http://b.turbo.az/r/?p=33" width="230"></iframe>
-				</div>
-				<div class="headerReklami">
-					<iframe border="0" frameborder="0" height="120" marginheight="0" marginwidth="0" scrolling="no" src="http://b.turbo.az/r/?p=34" width="230"></iframe>
-				</div>
-				<div class="headerReklami">
-					<iframe border="0" frameborder="0" height="120" marginheight="0" marginwidth="0" scrolling="no" src="http://b.turbo.az/r/?p=207" width="230"></iframe>
-				</div>
-			</div>
-
-			<!-- Header Menu (Dil seçimi və partnor saytlar) -->
-			<div class="headerMenu clearFix">
-				<a href="">bash sehife</a>
-				<ul>
-					<li id="az"><a>azerbaycanca</a></li>
-					<li id="ru"><a>по-русски</a></li>
-					<li id="en"><a>in english</a></li>
-				</ul>
-				<a href="">saytda reklam</a>
-			</div>
-		</div>
-	</header>
+	<?php include "header.php"; ?>
 	<!-- # Header End -->
 
 	<!-- Categories -->
@@ -555,12 +505,12 @@
 
 			<!-- Premium List Vac-->
 
-			<div class="headListLastVac">
+			<div class="mobile headListLastVac">
 				<div class="leftHead">
 					<h1>PREMİUM İŞ ELANLARI</h1>
 				</div>
 			</div>
-			<div class="listLastVac clearFix">
+			<div class="mobile listLastVac clearFix">
 				<div class="row">
 					<div class="col-md-4 first">
 						<a href="#vac" class="nameVac">Satış Təmsilçisi</a>
@@ -761,110 +711,4 @@
 	<!-- Last Vakancies End -->
 
 	<!-- Footer -->
-	
-	<section id="footer">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4">
-					<div class="footNav">
-						<ul class="footNavbar">
-							<li class="footNavbarItem mobile"><a href="">Mobil Versiyası</a></li>
-							<li class="footNavbarItem"><a href="">Haqqımızda</a></li>
-							<li class="footNavbarItem"><a href="">Saytda reklam</a></li>
-						</ul>
-					</div>
-					<div class="footInfo">
-						<ul class="info">
-							<li class="footCopy"><span>2008-2016 Boss.Az</span></li>
-							<li class="footPhone"><a href="">(012) 000-00-00</a></li>
-							<li class="footEmail"><a href="">boss@boss.az</a></li>
-						</ul>
-					</div>
-					<div class="copyright">
-						<span>
-							Saytın rəhbərliyi reklam bannerlərinin və yerləşdirilmiş elanların məzmununa görə məsuliyyət daşımır.
-						</span>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<a class="title" href="">İş elanları</a>
-					<ul class="footCatList">
-						<li class="cat"><a href="">Category Name</a></li>
-						<li class="cat"><a href="">Category Name</a></li>
-						<li class="cat"><a href="">Category Name</a></li>
-						<li class="cat"><a href="">Category Name</a></li>
-						<li class="cat"><a href="">Category Name</a></li>
-						<li class="cat"><a href="">Category Name</a></li>
-						<li class="cat"><a href="">Category Name</a></li>
-					</ul>
-				</div>
-				<div class="col-md-4">
-					<a class="title" href="">İş axtaranlar</a>
-					<ul class="footCatList">
-						<li class="cat"><a href="">Category Name</a></li>
-						<li class="cat"><a href="">Category Name</a></li>
-						<li class="cat"><a href="">Category Name</a></li>
-						<li class="cat"><a href="">Category Name</a></li>
-						<li class="cat"><a href="">Category Name</a></li>
-						<li class="cat"><a href="">Category Name</a></li>
-						<li class="cat"><a href="">Category Name</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</section>
-
-	<!-- Footer End -->
-
-	<!-- Jquery -->
-	<script type="text/javascript" src="js/bootstrap.js"></script>
-	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
-</body>
-</html>
-<script type="text/javascript">
-	// Dil seçimi
-	$(document).ready(function(){
-		$("#az").addClass("lang-selected");
-		$("#az").click(function(){
-			$(".lang-selected").removeClass("lang-selected");
-			$(this).addClass("lang-selected");
-		})
-		$("#ru").click(function(){
-			$(".lang-selected").removeClass("lang-selected");
-			$(this).addClass("lang-selected");
-		})
-		$("#en").click(function(){
-			$(".lang-selected").removeClass("lang-selected");
-			$(this).addClass("lang-selected");
-		})
-	});
-	// Tab menu (Vakansiyalar və CV-lər)
-	var thisAttr; 
-
-	$(document).ready(function() {
-
-    $(".catNav div a").click(function(){
-    	$(".active").removeClass("active");
-    	$(this).addClass("active");
-    	thisAttr = $(this).attr("href");
-    	$(".show").removeClass("show");
-    	$(thisAttr+"> .container").addClass("show");
-    });
-		
-	});
-
-	// Yuxarıdan aşağı açılan Axtarış sistemi
-	var height;
-	$(document).ready(function() {
-		height = $("#search > .container").height();
-		$("#search").hide();
-
-		$("#searchBtn").on('click', function(event){ 
-         	$('#search').slideToggle(100);
-    	});
-    	$("#searchBtn").click(function(){
-	    	$("#upDown").toggleClass("fa-caret-up");
-    	});
-	});
-
-</script>
+	<?php include "footer.php"; ?>
